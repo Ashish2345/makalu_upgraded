02f7204183ee08c7e228a00f6bbee58a43947d99
unknown <aashish>
2023-12-09 23:27:23 +0545

    second commit

--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -7,5 +7,18 @@ app_name = "frontend"
 
 urlpatterns = [
     path('', cache_page(0*60)(HomeView.as_view()), name="home_view"),
+    path('about-us/', AboutUsView.as_view(), name="about_us"),
+    path('contact-us/', ContactUsView.as_view(), name="contact_us"),
 
+    path('peak/lists/', cache_page(0*60)(PeeksListsView.as_view()), name="peek-lists"),
+
+    path('peek/details/<int:id>/', cache_page(0*60)(ToursDetailsView.as_view()), name="tour_details"),
+
+    path('blogs/', BlogsView.as_view(), name="blogs"),
+    path('blogs/details/<int:id>/', BlogDetailsView.as_view(), name="blog_details"),
+
+    path('terms-and-conditions/', TermsandConditionView.as_view(), name="terms"),
+    path('trip-advisory/', TripAdvisory.as_view(), name="trip-advisory"),
+
+    path('faq/', FAQView.as_view(), name="faq"),
 ]
--- src/frontend/views.py
+++ src/frontend/views.py
@@ -28,3 +28,146 @@ class HomeView(View):
         NewsLetterModel.objects.get_or_create(email=email)
         return JsonResponse({"sucesss": True})
 
+
+class AboutUsView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "about.html"
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class ContactUsView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "contact.html"
+        self.args = {}
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        data = request.POST.dict()
+        data.pop("csrfmiddlewaretoken")
+        contact = ContactUsModel(**data)
+        contact.save()
+        if contact:
+            self.args = {
+                "success": "Thank you for contacting Malaku Mountaineering! Your message has been successfully sent."
+            }   
+        return render(request, self.template_name, self.args)
+
+
+
+class PeeksListsView(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "peeklists.html"
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+
+class ToursDetailsView(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "details.html"
+        return super().dispatch(request, *args, **kwargs)
+    
+    def data_get(self, tours_details):
+        comments = CommentsTours.objects.filter(peek_info=tours_details)
+        return comments
+    
+    def get(self, request, *args, **kwargs):
+        id = kwargs.get("id")
+        # tours_details = get_object_or_404(PeeksLists, id=id)
+        self.args = {
+            # "tour":tours_details,
+            # "highlight": tours_details.peek_highlights.first(),
+            # "itemary": tours_details.peek_itemary,
+            # "comment": self.data_get(tours_details)
+        }
+
+        return render(request, self.template_name, self.args)
+    
+    def post(self, request, *args, **kwargs):
+        id = kwargs.get("id")
+        tours_details = get_object_or_404(PeeksLists, id=id)
+        data = request.POST.dict()
+        data.pop('csrfmiddlewaretoken', None)
+
+        if "comments" in request.POST:
+            data.pop('comments', None)
+            comment = CommentsTours(**data, peek_info=tours_details)
+            comment.save()
+            message = "Comment Added Successfully!"
+        else:
+            data.pop('book_tour', None)
+            print(data)
+            tour = BookaTour(**data, peek_info=tours_details)
+            tour.save()
+            message = "Booked Successfully!"
+
+        self.args = {
+            "tour":tours_details,
+            "highlight": tours_details.peek_highlights.first(),
+            "itemary": tours_details.peek_itemary.first(),
+            "message":message,
+            "comment": self.data_get(tours_details)
+        }
+        return render(request, self.template_name, self.args)
+
+
+class BlogsView(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "blogs.html"
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        # blog_lists = Blogs.objects.all()
+        return render(request, self.template_name)
+    
+
+class BlogDetailsView(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "blogs-details.html"
+        self.args = {}
+        return super().dispatch(request, *args, **kwargs)
+    
+
+
+    def get(self, request, *args, **kwargs):
+            # id = kwargs.get("id")
+            # blogs = get_object_or_404(Blogs, id=id)
+            # self.args = {
+            #     "blog":blogs
+            # }
+            return render(request, self.template_name, self.args)
+
+
+class TermsandConditionView(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "terms.html"
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class TripAdvisory(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "trip_advisory.html"
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        
+        return render(request, self.template_name)
+
+class FAQView(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = "faq.html"
+        self.payload = {}
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
